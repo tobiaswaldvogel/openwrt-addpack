fafbbe57a0b8f054e5bd263df8e52a705840afbc
Tobias Waldvogel <tobias.waldvogel@trustanywhere.com>
2013-01-03 16:29:40 +0100

    Move mediatomb to taglib

--- luci-app-hd-idle2/files/root/usr/lib/lua/luci/model/cbi/hd-idle.lua
+++ luci-app-hd-idle2/files/root/usr/lib/lua/luci/model/cbi/hd-idle.lua
@@ -6,32 +6,32 @@ LuCI Spindown
 
 require "luci.fs"
 
-m = Map("hd-idle", translate("hd-idle_title"), translate("hd-idle_desc"))
+m = Map("hd-idle", translate("HD idle"), translate("Spin down disks after a period of idle time."))
 
 m:section(SimpleSection).template = "hd/disc_power_status"
 
-s = m:section(TypedSection, "hd-idle", translate("hd-idle_settings"))
+s = m:section(TypedSection, "hd-idle", translate("Settings"))
 s.addremove = true
 s.anonymous = true
 
-s:option(Flag, "enabled", translate("hd-idle_enable"))
+s:option(Flag, "enabled", translate("Enable"))
 
-disk = s:option(Value, "disk", translate("hd-idle_disk"))
+disk = s:option(Value, "disk", translate("Disk"))
 disk.rmempty = false
 for _, dev in ipairs(luci.fs.glob("/dev/sd?")) do
 	disk:value(dev:sub(6))
 end
 
-idle = s:option(Value, "idle_time_interval", translate("hd-idle_idle_time_interval"))
+idle = s:option(Value, "idle_time_interval", translate("Idle time"))
 idle.rmempty = true
 idle.size = 4
 
-unit = s:option(ListValue, "idle_time_unit", translate("hd-idle_idle_time_unit"))
+unit = s:option(ListValue, "idle_time_unit", translate("Unit"))
 unit.default = "minutes"
-unit:value("seconds", translate("hd-idle_seconds"))
-unit:value("minutes", translate("hd-idle_minutes"))
-unit:value("hours", translate("hd-idle_hours"))
-unit:value("days", translate("hd-idle_days"))
+unit:value("seconds", translate("Seconds"))
+unit:value("minutes", translate("Minutes"))
+unit:value("hours", translate("Hours"))
+unit:value("days", translate("Days"))
 unit.rmempty = false
 
 return m
--- luci-app-hd-mon/files/root/usr/lib/lua/luci/model/cbi/hd-mon.lua
+++ luci-app-hd-mon/files/root/usr/lib/lua/luci/model/cbi/hd-mon.lua
@@ -4,7 +4,7 @@ LuCI hdmon
 
 ]]--
 
-m = Map("hd-mon", translate("hdmon"), translate("hdmon_desc"))
+m = Map("hd-mon", translate("Hard disk monitoring"), translate("Monitors disks and raid arrays and sends an email in case of failure."))
 
 local fs = require "luci.fs"
 local devices = {}
@@ -13,15 +13,15 @@ luci.util.update(devices, fs.glob("/dev/sd?") or {})
 m:section(SimpleSection).template = "hd/raid_status"
 m:section(SimpleSection).template = "hd/disc_temp_status"
 
-s = m:section(TypedSection, "raid", translate("raid"))
+s = m:section(TypedSection, "raid", translate("Raid arrays"))
 s.anonymous = true
-s:option(Flag, "enabled", translate("enable_mon"))
+s:option(Flag, "enabled", translate("Enable monitoring"))
 
-s = m:section(TypedSection, "smart", translate("smart"))
+s = m:section(TypedSection, "smart", translate("S.M.A.R.T."))
 s.anonymous = true
 s.addremove = true
-s:option(Flag, "enabled", translate("enable_mon"))
-disk = s:option(Value, "disk", translate("disk"))
+s:option(Flag, "enabled", translate("Enable monitoring"))
+disk = s:option(Value, "disk", translate("Disk"))
 disk.rmempty = fales
 for _, dev in ipairs(devices) do
 	disk:value(dev)
@@ -32,18 +32,18 @@ temp_info.default = "40"
 temp_warn = s:option(Value, "temp_warn", translate("Threshold for temperature warning"))
 temp_warn.default = "45"
 
-s = m:section(TypedSection, "hdmon", translate("monreceiver"))
+s = m:section(TypedSection, "hdmon", translate("Notify options"))
 s.anonymous = true
-s:option(Value, "email", translate("email", "Email"))
-s:option(Flag, "test", translate("email_test"))
-s = m:section(TypedSection, "ssmtp", translate("monsender"))
+s:option(Value, "email", translate("Email address to notify"))
+s:option(Flag, "test", translate("Send test mail"))
+s = m:section(TypedSection, "ssmtp", translate("Mail options"))
 s.anonymous = true
-sender = s:option(Value, "mx", translate("mx"))
+sender = s:option(Value, "mx", translate("Mail server"))
 sender:value("smtp.gmail.com:587")
-s:option(Value, "sender", translate("mailsender"))
-s:option(Value, "user", translate("mailuser"))
-pwd = s:option(Value, "pwd", translate("mailpwd"))
+s:option(Value, "sender", translate("Sender address"))
+s:option(Value, "user", translate("User ID"))
+pwd = s:option(Value, "pwd", translate("Password"))
 pwd.password = true
-s:option(Flag, "TLS", translate("usetls"))
+s:option(Flag, "TLS", translate("Use TLS"))
 
 return m
